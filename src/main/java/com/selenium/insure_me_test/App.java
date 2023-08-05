2b847a0c3b37cb08ddbf2fd78219a3d497f72208
Mohan <mohan.devops019@gmail.com>
2023-08-05 15:08:58 -0400

    updated selenium script

--- src/main/java/com/selenium/insure_me_test/App.java
+++ src/main/java/com/selenium/insure_me_test/App.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import java.time.Duration;
 
 /**
  * Hello world!
